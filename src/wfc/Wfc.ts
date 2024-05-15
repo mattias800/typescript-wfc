84e49413204dba6cd44f061f31c8c0ca9700db00
Mattias Andersson <mattias800@gmail.com>
2024-05-15 21:09:57 +0200

    - Add extractor that can extract wcf rules from tilemap.

---
+++ src/wfc/Wfc.ts
@@ -0,0 +1,64 @@
+export type TileId = string;
+export type SourceMap = Array<Array<TileId>>;
+
+export interface RuleSet {
+  [key: string]: AllowedNeighbours;
+}
+
+export interface AllowedNeighbours {
+  right: Array<string>;
+  left: Array<string>;
+  up: Array<string>;
+  down: Array<string>;
+}
+
+export const extractRuleSet = (source: SourceMap): RuleSet => {
+  const rows = source.length;
+  const cols = source[0].length;
+
+  const ruleSet: RuleSet = {};
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const tile = source[row][col];
+
+      ruleSet[tile] = ruleSet[tile] ?? {
+        left: [],
+        down: [],
+        right: [],
+        up: [],
+      };
+
+      if (col > 0) {
+        let t = source[row][col - 1];
+        if (!ruleSet[tile].left.includes(t)) {
+          ruleSet[tile].left.push(t);
+          ruleSet[tile].left.sort()
+        }
+      }
+      if (col < cols - 1) {
+        let t = source[row][col + 1];
+        if (!ruleSet[tile].right.includes(t)) {
+          ruleSet[tile].right.push(t);
+          ruleSet[tile].right.sort();
+        }
+      }
+      if (row > 0) {
+        let t = source[row - 1][col];
+        if (!ruleSet[tile].up.includes(t)) {
+          ruleSet[tile].up.push(t);
+          ruleSet[tile].up.sort();
+        }
+      }
+      if (row < rows - 1) {
+        let t = source[row + 1][col];
+        if (!ruleSet[tile].down.includes(t)) {
+          ruleSet[tile].down.push(t);
+          ruleSet[tile].down.sort();
+        }
+      }
+    }
+  }
+
+  return ruleSet;
+};
---
+++ src/wfc/__tests__/Wtc.test.ts
@@ -0,0 +1,77 @@
+import { extractRuleSet } from "../Wfc.ts";
+
+describe("Wcf", () => {
+  describe("extractRuleSet", () => {
+    describe("2x2 example", () => {
+      const r = extractRuleSet([
+        ["1", "1"],
+        ["1", "2"],
+      ]);
+
+      it("extracts all tiles", () => {
+        expect(Object.keys(r)).toEqual(["1", "2"]);
+      });
+
+      it("extracts left tiles", () => {
+        expect(r["1"].left).toEqual(["1"]);
+        expect(r["2"].left).toEqual(["1"]);
+      });
+
+      it("extracts right tiles", () => {
+        expect(r["1"].right).toEqual(["1", "2"]);
+        expect(r["2"].right).toEqual([]);
+      });
+
+      it("extracts up tiles", () => {
+        expect(r["1"].up).toEqual(["1"]);
+        expect(r["2"].up).toEqual(["1"]);
+      });
+
+      it("extracts down tiles", () => {
+        expect(r["1"].down).toEqual(["1", "2"]);
+        expect(r["2"].down).toEqual([]);
+      });
+    });
+
+    describe("4x4 example", () => {
+      const r = extractRuleSet([
+        ["4", "1", "1", "1"],
+        ["1", "2", "2", "3"],
+        ["1", "2", "3", "3"],
+        ["1", "3", "3", "3"],
+      ]);
+
+      it("extracts all tiles", () => {
+        expect(Object.keys(r)).toEqual(["1", "2", "3", "4"]);
+      });
+
+      it("extracts left tiles", () => {
+        expect(r["1"].left).toEqual(["1", "4"]);
+        expect(r["2"].left).toEqual(["1", "2"]);
+        expect(r["3"].left).toEqual(["1", "2", "3"]);
+        expect(r["4"].left).toEqual([]);
+      });
+
+      it("extracts right tiles", () => {
+        expect(r["1"].right).toEqual(["1", "2", "3"]);
+        expect(r["2"].right).toEqual(["2", "3"]);
+        expect(r["3"].right).toEqual(["3"]);
+        expect(r["4"].right).toEqual(["1"]);
+      });
+
+      it("extracts up tiles", () => {
+        expect(r["1"].up).toEqual(["1", "4"]);
+        expect(r["2"].up).toEqual(["1", "2"]);
+        expect(r["3"].up).toEqual(["1", "2", "3"]);
+        expect(r["4"].up).toEqual([]);
+      });
+
+      it("extracts down tiles", () => {
+        expect(r["1"].down).toEqual(["1", "2", "3"]);
+        expect(r["2"].down).toEqual(["2", "3"]);
+        expect(r["3"].down).toEqual(["3"]);
+        expect(r["4"].down).toEqual(["1"]);
+      });
+    });
+  });
+});
