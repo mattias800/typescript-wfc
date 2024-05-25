987c048628254d0e23bc8735df822145653ec0db
Mattias Andersson <mattias800@gmail.com>
2024-05-25 22:22:12 +0200

    - Add react-remember.

--- src/Store.ts
+++ src/Store.ts
@@ -3,13 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { tileAtlasSlice } from "./app/tile-atlas/TileAtlasSlice.ts";
 import { tileAtlasImporterSlice } from "./app/tile-atlas-importer/TileAtlasImporterSlice.ts";
 import { wfcSlice } from "./app/wfc-ruleset/WfcSlice.ts";
+import { rememberReducer, rememberEnhancer } from "redux-remember";
 
 export const store = configureStore({
-  reducer: {
+  reducer: rememberReducer({
     tileAtlas: tileAtlasSlice.reducer,
     tileAtlasImporter: tileAtlasImporterSlice.reducer,
     wfc: wfcSlice.reducer,
-  },
+  }),
+  enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(
+      rememberEnhancer(
+        window.localStorage, // or window.sessionStorage, or your own custom storage driver
+        ["tileAtlas", "tileAtlasImporter", "wfc"],
+      ),
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
